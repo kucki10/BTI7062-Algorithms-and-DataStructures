75c544df30a4871885967029f89637edef1e8df6
Christian Nussbaum <christian.nussbaum@protonmail.ch>
2018-10-27 11:19:21 +0200

    Reverted to old ParallelisedandConquerable

--- 06-homework/Algorithms/src/algorithms/templates/ParallelisedDivideAndConquerable.java
+++ 06-homework/Algorithms/src/algorithms/templates/ParallelisedDivideAndConquerable.java
@@ -1,14 +1,11 @@
 package algorithms.templates;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.RejectedExecutionException;
 import java.util.concurrent.ThreadPoolExecutor;
-
-import org.apache.commons.lang3.concurrent.ConcurrentUtils;
-
 
 public interface ParallelisedDivideAndConquerable<OutputType> extends DivideAndConquerable<OutputType> {
 
@@ -16,33 +13,28 @@ public interface ParallelisedDivideAndConquerable<OutputType> extends DivideAndC
     List<? extends ParallelisedDivideAndConquerable<OutputType>> decompose();
 
     default OutputType divideAndConquer(ThreadPoolExecutor executor) {
-
         if (this.isBasic()) {
             return this.baseFun();
         }
 
         List<? extends ParallelisedDivideAndConquerable<OutputType>> subComponents = this.decompose();
 
-        List<OutputType> intermediateResults = new Vector<>(subComponents.size());
-        List<Future<OutputType>> futureResults = new Vector<>(subComponents.size());
+        List<OutputType> intermediateResults = new ArrayList<OutputType>(subComponents.size());
+        List<Future<OutputType>> futureResults = new ArrayList<>(subComponents.size());
 
         subComponents.forEach(subComponent-> {
             try {
-                futureResults.add(executor.submit(() -> {
-                    return subComponent.divideAndConquer(executor);
-                }));
+                futureResults.add(executor.submit(() -> subComponent.divideAndConquer(executor)));
             } catch (RejectedExecutionException exception) {
-                //NOTE: Do not add them to immediate results
-                //Because this would sometimes mix up the ordering (left would be right)
-                //Which can cause IndexOutOfBound Exceptions
-                futureResults.add(ConcurrentUtils.constantFuture(subComponent.divideAndConquer(executor)));
+                intermediateResults.add(subComponent.divideAndConquer(executor));
             }
         });
 
-      futureResults.forEach(x -> {
+        futureResults.forEach(x -> {
             try {
                 intermediateResults.add(x.get());
             } catch (ExecutionException | InterruptedException exception) {
+                System.out.println("something went wrong");
                 exception.printStackTrace();
             }
         });
