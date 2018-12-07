d82a840e0165c6be22b731f1a50e314c0b7acd73
Christian Nussbaum <christian.nussbaum@protonmail.ch>
2018-12-07 17:41:40 +0100

    Added Matrices

--- 08-homework/Algorithms/src/algorithms/templates/Matrix.java
+++ 08-homework/Algorithms/src/algorithms/templates/Matrix.java
@@ -1,8 +1,11 @@
 package algorithms.templates;
 
 public interface Matrix<T> {
-
-    Matrix<T> calculate();
-
-
+    T[][] getMatrix();
+    int getWidth();
+    int getHeight();
+    void setValue(int column, int row, T value);
+    T getValue(int column, int row);
+    Matrix<T> multiply(Matrix<T> matrix);
+    Matrix<T> potentiate(int n);
 }
